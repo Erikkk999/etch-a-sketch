c5ff50ea373d8816d65e5a5bd234f4f2a4996408
Erikkk999 <193602698+Erikkk999@users.noreply.github.com>
2026-03-18 19:47:15 +0100

    feat: add button and logic for grid resizing

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 const gridContainer = document.querySelector(".grid-container");
-const gridCount = 256;
+const resizeBtn = document.querySelector(".resize");
 
-for (let i = 0; i < gridCount; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    gridContainer.appendChild(square);
-}
+let drawing = false;
 
-gridContainer.addEventListener("mouseover", (e) => {
+gridContainer.addEventListener("mousedown", (e) => {
+    e.preventDefault();
     if (e.target.classList.contains("square")) {
+        drawing = true;
         e.target.classList.add("hover");
     }
+});
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (drawing && e.target.classList.contains("square")) {
+        e.target.classList.add("hover");
+    }
+});
+
+window.addEventListener("mouseup", () => {
+    drawing = false;
+});
+
+resizeBtn.addEventListener("click", () => {
+    const input = prompt("Enter a number between 2 - 100");
+    if (input === null) return;
+
+    const gridSize = parseInt(input);
+    if (isNaN(gridSize) || gridSize > 100 || gridSize <= 1) return;
+
+    gridContainer.innerHTML = "";
+    gridContainer.style.setProperty("--column", gridSize);
+
+    const fullGrid = gridSize * gridSize;
+
+    for (let i = 0; i < fullGrid; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        gridContainer.appendChild(square);
+    }   
 });
