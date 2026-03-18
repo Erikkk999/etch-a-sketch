1e56c786bc9df0681348292d01818306b18fade7
Erikkk999 <193602698+Erikkk999@users.noreply.github.com>
2026-03-18 14:48:02 +0100

    feat: set up hover effect on divs
    the divs change color when the mouse passes over them.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const divContainer = document.querySelector(".grid-container");
+const gridContainer = document.querySelector(".grid-container");
 const gridCount = 256;
 
 for (let i = 0; i < gridCount; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    divContainer.appendChild(square);
+    gridContainer.appendChild(square);
 }
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("square")) {
+        e.target.classList.add("hover");
+    }
+});
